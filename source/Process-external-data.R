029cb33e63d14b571cc7b0c3ee1fe7b8549bcf68
davidlord <87085423+davidlord@users.noreply.github.com>
2022-01-21 11:29:50 +0100

    Update Process-external-data.R

--- source/Process-external-data.R
+++ source/Process-external-data.R
@@ -1,20 +1,39 @@
 library(dplyr)
 library(tidyverse)
+library(stringr)
 
 # Set working directory (also place data to read in working directory).
-  WORK_DIR <- "/Users/davidlord/Documents/External_data"
-  setwd(WORK_DIR)
+WORK_DIR <- "/Users/davidlord/Documents/External_data"
+setwd(WORK_DIR)
 
 # Read all .tsv files in the folder into a list
-  MYFILES <- list.files(path = WORK_DIR, 
+MYFILES <- list.files(path = WORK_DIR, 
                      pattern = "\\.tsv$")
 
 # Read all files in the list
-  MYDATA <- lapply(MYFILES, read.delim)
+MYDATA <- lapply(MYFILES, read.delim)
 
 # Convert the read files from lists to data frames
 
-# SOLVE THIS :)
+# Get column names of all data frames in list, store in object
+
+  # Count number of lists in MYDATA
+  LEN <- (length(MYDATA))
+
+  # Count number of columns in total in all dataframes in MYDATA
+  COL_COUNT = 0
+  for (i in 1:LEN) {
+      N <- (length(MYDATA[[i]]))
+      COL_COUNT = COL_COUNT + N
+      }
+
+    # Create an empty list with the length of COL_COUNT  
+    COL_LIST <- vector(length = COL_COUNT)
+    
+for(i in 1:LEN){
+  test_colnames_list <- (colnames(MYDATA[[i]]))
+}
+
 
 # Print the column names of all dataframes and store in object. 
 
