30756e09b44dcdf2edeb119c409f5fc135688a45
davidlord <87085423+davidlord@users.noreply.github.com>
2022-01-21 10:01:08 +0100

    Create Manual-processing-external-data.R

---
+++ source/Manual-processing-external-data.R
@@ -0,0 +1,10 @@
+library(dplyr)
+library(tidyverse)
+
+# Set working directory (also place data to read in working directory).
+  WORK_DIR <- "/Users/davidlord/Documents/External_data"
+  setwd(WORK_DIR)
+  
+  
+
+
