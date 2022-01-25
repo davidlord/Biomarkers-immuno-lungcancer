718494e2e9cbaae5aeee1db2a75b9a16af2b4179
davidlord <david.lord96@gmail.com>
2022-01-25 18:34:30 +0100

    added notes to histogram.R
    added notes to histogram.R

--- source/External_data_processing/external_clinical_data_processing.R
+++ source/External_data_processing/external_clinical_data_processing.R
@@ -1,6 +1,7 @@
 library(dplyr)
 library(tidyverse)
 library(stringr)
+library(writexl)
 
 # Set working directory (also place data to read in working directory).
 WORK_DIR <- "/Users/davidlord/Documents/External_data"
@@ -33,5 +34,15 @@ MYDATA <- lapply(MYFILES, read.delim)
       ### UNIQUE_COLNAMES <- COLNAMES[unique_ind]
 
       ### length(UNIQUE_COLNAMES)
+
+
+# Store data frame in object, manual for now, update later... 
+df1 <- as.data.frame(MYDATA[[1]])
+df2 <- as.data.frame(MYDATA[[2]])
+df3 <- as.data.frame(MYDATA[[3]])
+df4 <- as.data.frame(MYDATA[[4]])
+
+# Create an excel table from the data frame
+write_xlsx(df1, "/Users/davidlord/Documents/External_data/Test_data_frames/test_table.xlsx")
 
 
--- source/Visualisation/histogram.R
+++ source/Visualisation/histogram.R
@@ -11,13 +11,18 @@ setwd(WORK_DIR)
 # Pipe the data to ggplot, read the '% MSI' column as numerical data, store in object MSI
 MSI <- msi_status_biolung %>% ggplot(aes(as.numeric(msi_status_biolung$`% MSI`))) +
     # Set geometry to histogram
-    geom_histogram(binwidth = 1) +
+    geom_histogram(binwidth = 1, fill = "darkblue", color = "gray") +
     # Add xlabel
     xlab("% MSI") +
     # Add title
     ggtitle("% MSI histogram") +
-    # Move title to the middle of the plot
-    theme(plot.title = element_text(hjust = 0.5))
+    # Set theme
+    theme_economist()
 
 # Print plot
 MSI
+
+# FIX: Enable both economist theme and adjust title to be in the center of the plot. 
+### theme(plot.title = element_text(hjust = 0.5))
+
+# May want to edit plot so that patients with positive vs. negative treatment outcome have different colors in histogram. 
