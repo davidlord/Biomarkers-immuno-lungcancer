f5821d9c97afabe287405bab089423e1275fb82d
David Lord <xlorda@medair1.medair.lcl>
2022-02-11 10:48:37 +0100

    updated requirements, installed bfctools

--- source/somatic_variant_calling/somatic-variant-calling-wrapper.sh
+++ source/somatic_variant_calling/somatic-variant-calling-wrapper.sh
@@ -1,6 +1,35 @@
 #!/bin/bash
 
 # Wrapper script for somatic variant calling and variant filtering, script calls: X, Y, Z, I, N. 
-# INPUT: List (text file) of processed BAM files.
+# INPUT: List (text file) of normal-tumor (N-T) paired processed BAM files; N T.
 # 
+
+
+
+
+
+INPUT=$1
+
+# Create two lists, one containing each N entry of the input file and one containing each T entry of the input file. 
+
+
+
+IN_FILES=""
+while read N T; do
+	N_LIST+=`echo $N`
+	T_LIST+=`echo $T`
+done < $INPUT
+
+
+## echo N_LIST: $N_LIST
+## echo T_LIST: $T_LIST
+
+while read N; do
+	FIRST=`echo $N | tr '\n' ' '`
+	N2_LIST=+=($FIRST)
+done < $N_LIST
+
+
+# echo N2_LIST: $N2_LIST
+
 
