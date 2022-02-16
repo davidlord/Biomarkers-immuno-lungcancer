77433d3957b0fd03da954a8aa4c394aaebfff539
davidlord <87085423+davidlord@users.noreply.github.com>
2022-02-16 13:04:50 +0100

    Update extract-funcotations.py

---
+++ source/vcf_parsing/extract-funcotations.py
@@ -0,0 +1,17 @@
+# Set python as interpreter
+
+
+# Read .vcf file input line by line, or possibly as data frame??
+
+# Create new data frame in which data will be added, add headers to data frame. 
+
+# Skip header lines ("##"). 
+
+# Skip lines that did not PASS. 
+
+# Select INFO field for each line. 
+
+# Select FUNCOTATION within INFO field. 
+
+# Select the data inside FUNCOTATION, add to new data frame. 
+
