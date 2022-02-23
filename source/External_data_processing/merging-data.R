dc536e38f4b98d1b1423239b05f0a6371a33b4af
davidlord <david.lord96@gmail.com>
2022-02-23 15:46:39 +0100

    Added py script for merging of external data
    Added

--- source/External_data_processing/merging-data.R
+++ source/External_data_processing/merging-data.R
@@ -1,11 +1,30 @@
+library(dplyr)
+library(tidyverse)
+library(stringr)
+library(writexl)
 
+# Read mutation table files
+# Set working directory (also place data to read in working directory).
+WORK_DIR <- "/Users/davidlord/Documents/External_data/script_running"
+setwd(WORK_DIR)
+
+# Read all .tsv files in the folder into a list
+MYFILES <- list.files(path = WORK_DIR, 
+                      pattern = "\\.mutation.tsv$")
+# Read all files in the list
+MYDATA <- lapply(MYFILES, read.delim)
+
+df <- as.data.frame(MYDATA[[1]])
+
+
+help("names")
 # Load clinical data tables as data frames. 
 
   # Rename columns to be included so they match. 
 
   # Select columns, then merge clinical data tables to one data set. 
 
-  # Rename entries in columns using 'ifelse'. 
+  # Rename entries i n columns using 'ifelse'. 
 
 
 # For each mutation tables: 
