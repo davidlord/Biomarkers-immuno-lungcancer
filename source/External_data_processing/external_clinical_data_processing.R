a25334e4e43259976e8e7b9ee86424b13680f56e
davidlord <david.lord96@gmail.com>
2022-03-10 14:39:55 +0100

    Updated external clinical data processing script

--- source/External_data_processing/external_clinical_data_processing.R
+++ source/External_data_processing/external_clinical_data_processing.R
@@ -7,68 +7,88 @@ library(writexl)
 WORK_DIR <- "/Users/davidlord/Documents/External_data/script_running"
 setwd(WORK_DIR)
 
-# Read all .tsv files in the folder into a list
-MYFILES <- list.files(path = WORK_DIR, 
+# Read all tsv files or read manually: 
+  
+  # Read all .tsv files: 
+
+    # Read all .tsv files in the folder into a list
+    MYFILES <- list.files(path = WORK_DIR, 
                       pattern = "\\.tsv$")
-# Read all files in the list
-MYDATA <- lapply(MYFILES, read.delim)
+    # Read all files in the list
+    MYDATA <- lapply(MYFILES, read.delim)
 
-
-# Read each data sets as data frames
-df1 <- as.data.frame(MYDATA[[1]])
-df2 <- as.data.frame(MYDATA[[2]])
-df3 <- as.data.frame(MYDATA[[3]])
-df4 <- as.data.frame(MYDATA[[4]])
-df5 <- as.data.frame(MYDATA[[5]])
+    # Read each data sets as data frames
+    df1 <- as.data.frame(MYDATA[[1]])
+    df2 <- as.data.frame(MYDATA[[2]])
+    df3 <- as.data.frame(MYDATA[[3]])
+    df4 <- as.data.frame(MYDATA[[4]])
+    df5 <- as.data.frame(MYDATA[[5]])
+    df6 <- as.data.frame(MYDATA[[6]])
+    
+  # Read manually
+    Rivzi_2015 <- read.delim("Rivzi_2015_clinical_data.tsv")
+    LUAD_Rivzi_2015 <- read.delim("LUAD_Rivzi_2015_clinical_data.tsv")
+    Jordan_2017 <- read.delim("Jordan_2017_clinical_data.tsv")
+    Hellmann_2018 <- read.delim("Hellmann_2018_clinical_data.tsv")
+    Rivzi_2018 <- read.delim("Rivzi_2018_clinical_data.tsv")
+    Chen_2020 <- read.delim("LUAD_Chen_2020_clinical_data.tsv")
+    
+  
 
 # Select relevant columns and filter for immunotherapy when necessary. 
 Col_in_all_datasets <- c("Study.ID", "Patient.ID", "Sample.ID", "Cancer.Type.Detailed", "Durable.Clinical.Benefit", "Sex", "TMB..nonsynonymous.")
 # Add: Age, smoking status
 
-df1_trimmed <- df1 %>% select("Study.ID", "Age..yrs.", "Smoking.Status", "Patient.ID", "Sample.ID", "Cancer.Type.Detailed", "Durable.Clinical.Benefit", "Sex", "TMB..nonsynonymous.")
+Hellmann_2018_trimmed <- Hellmann_2018 %>% select("Study.ID", "Age..yrs.", "Smoking.Status", "Patient.ID", "Sample.ID", "Cancer.Type.Detailed", "Durable.Clinical.Benefit", "Sex", "TMB..nonsynonymous.", "Progress.Free.Survival..Months.", "PD.L1.expression..Percentage.")
 
-df2_trimmed <- df2 %>% filter(Immunotherapy == 'YES' & !is.na(Durable.Clinical.Benefit)) %>% select("Study.ID", "Smoking.History", "Diagnosis.Age", "Patient.ID", "Sample.ID", "Cancer.Type.Detailed", "Durable.Clinical.Benefit", "Sex", "TMB..nonsynonymous.")
+Jordan_2017_trimmed <- Jordan_2017 %>% filter(Immunotherapy == 'YES' & !is.na(Durable.Clinical.Benefit)) %>% select("Study.ID", "Smoking.History", "Diagnosis.Age", "Patient.ID", "Sample.ID", "Cancer.Type.Detailed", "Durable.Clinical.Benefit", "Sex", "TMB..nonsynonymous.", "Stage.At.Diagnosis", "Gene.Panel")
                               
-df3_trimmed <- df3 %>% select("Study.ID", "Diagnosis.Age", "Smoking.History", "Patient.ID", "Sample.ID", "Cancer.Type.Detailed", "Durable.Clinical.Benefit", "Sex", "TMB..nonsynonymous.",)
+LUAD_Rivzi_2015_trimmed <- LUAD_Rivzi_2015 %>% select("Study.ID", "Diagnosis.Age", "Smoking.History", "Patient.ID", "Sample.ID", "Cancer.Type.Detailed", "Durable.Clinical.Benefit", "Sex", "TMB..nonsynonymous.", "PDL1.Expression", "Progress.Free.Survival..Months.")
 
-df4_trimmed <- df4 %>% select("Study.ID", "Smoking.History", "Patient.Current.Age", "Patient.ID", "Sample.ID", "Cancer.Type.Detailed", "Durable.Clinical.Benefit", "Sex", "TMB..nonsynonymous.")
+Rivzi_2015_trimmed <- Rivzi_2015 %>% select("Study.ID", "Smoking.History", "Patient.Current.Age", "Patient.ID", "Sample.ID", "Cancer.Type.Detailed", "Durable.Clinical.Benefit", "Sex", "TMB..nonsynonymous.", "PDL1.Expression", "Progress.Free.Survival..Months.")
 
-df5_trimmed <- df5 %>% select("Study.ID", "Age..yrs.", "Smoking.Status", "Patient.ID", "Sample.ID", "Cancer.Type.Detailed", "Durable.Clinical.Benefit", "Sex", "TMB..nonsynonymous.")
+Rivzi_2018_trimmed <- Rivzi_2018 %>% select("Study.ID", "Patient.ID", "Sample.ID", "Diagnosis.Age", "Cancer.Type.Detailed", "Durable.Clinical.Benefit", "Gene.Panel", "PD.L1.Score....", "Progress.Free.Survival..Months.", "Sex", "Smoker", "TMB..nonsynonymous.")
 
-### NOTE: May add gene panels (info from publications) as column later, comparison of different properties between gene panels / WES
+# Exclude for now since does not contain progression free survival status
+Chen_2020_trimmed <- LUAD_Chen_2020 %>% filter(TKI.Treatment == 'Yes'& Sequencing.Type != 'RNA-Seq') %>% select("Cancer.Study", "Patient.ID", "Sample.ID", "Cancer.Type.Detailed", "Age", "Sex", "Stage", "Smoking.status", "TMB..nonsynonymous.")
+
 
 ### Select name conventions for columns: 
 
 ## Study ID = 'Study.ID'
 ## Patient ID = 'Patient.ID'
 ## Sample ID = 'Sample.ID'
-## Cancer type = 'Cancer.Type.Detailed'
+## Cancer type = 'Cancer.Type'
 ## Age = 'Age'
 ## Sex = 'Sex'
 ## TMB = 'TMB..nonsynonymous.'
 ## Smoking status = 'Smoking.Status'
 ## Clinical benefit = 'Durable.Clinical.Benefit'
+## Gene panel = Gene.Panel
+## PD.L1 score = PD-L1_score
+## Progression free survival = 'Progression.Free.Survival'
+
+# Change column names using: 
+colnames(df)[which(names(df) == "colname")] <- "newcolname"
+
+# Rename columns prior to data merge
+
+# Hellmann_2018
+colnames(Hellmann_2018_trimmed)[which(names(Hellmann_2018_trimmed) == "Age..yrs.")] <- "Diagnosis.Age"
+
+# Jordan_2017
+colnames(Jordan_2017_trimmed)[which(names(Jordan_2017_trimmed) == "Diagnosis.Age")] <- "Age"
 
 
-# Rename necessary columns
 
-# df1:
-colnames(df1_trimmed) [2] <- "Age"
+# LUAD_Rivzi_2015
 
-# df2:
-colnames(df2_trimmed) [2] <- "Smoking.Status"
-colnames(df2_trimmed) [3] <- "Age"
 
-# df3: 
-colnames(df3_trimmed) [2] <- "Age"
-colnames(df3_trimmed) [3] <- "Smoking.Status"
+# Rivzi_2015
+colnames(Hellmann_2018_trimmed)[which(names(Hellmann_2018_trimmed) == "Patient.Current.Age")] <- "Diagnosis.Age"
 
-# df4:
-colnames(df4_trimmed) [2] <- "Smoking.Status"
-colnames(df4_trimmed) [3] <- "Age"
+# Rivzi_2018
 
-# df5:
-colnames(df5_trimmed) [2] <- "Age"
 
 
 # Merge data sets by columns
