daa09c20b16192ad9b829253914728b405b80ac3
David Lord <xlorda@medair1.medair.lcl>
2022-03-11 10:36:55 +0100

    cleanup vcf parsing script dir

--- source/vcf_parsing/variants-df-generator.py
+++ source/vcf_parsing/variants-df-generator.py
@@ -1,18 +1,16 @@
 from cyvcf2 import VCF
-import click
 import pandas as pd
 import os
 import glob
 
-# Define gene list (will be moved to config file):
+# Define gene list
+	# Development: Move to config file.
 genes_of_interest = ['EGFR', 'BRAF', 'TP53', 'PAK3', 'ATRX']
 
 # Read all vcf files in cwd: 
+	# Development: Move WD to config file. 
 vcf_files_list = glob.glob("*.vcf")
 
-# TO DEBUG: 
-print("vcf files list: ")
-print(vcf_files_list)
 
 # Get sample names, store in list
 sample_names_list = []
@@ -20,45 +18,28 @@ for vcf_file in vcf_files_list:
 	samplename = vcf_file[:8]
 	sample_names_list.append(samplename)
 
-# TO DEBUG:
-print("sample names list: ")
-print(sample_names_list)
-
 
 # Create dataframe to place mutations in: 
 column_1_name = ["Patient.ID"]
 column_names = column_1_name + genes_of_interest
-
-# TO DEBUG: 
-print("Column names in df")
-print(column_names)
-
 df = pd.DataFrame(columns = column_names)
-print("df:")
-print(df)
-
-
-# Get genes and shiii as dict
 
 # append sample name + genes dict as rows to df. 
 
 # Parse through vcf files (cyvcf2): 
 for vcf_file in vcf_files_list:
-	# get sample name
+	# get sample name: 
+		# Development: Use split instead of hardcoding, update later. 
 	samplename = vcf_file[:8]
 	# Create dict to store mutated genes:
 	genes_sample_dict = {"Patient.ID": samplename}
-	print("dict:")
-	print(genes_sample_dict)
 	for variant in VCF(vcf_file):
 		if variant.FILTER != None:
 			continue
 		else:
-			info = variant.INFO.get('FUNCOTATION')
-			gene_name = info.split('|')[0][1:]
+			infoF = variant.INFO.get('FUNCOTATION')
+			gene_name = infoF.split('|')[0][1:]
 			if gene_name in genes_of_interest:
-				print(samplename, end = " ")
-				print(gene_name)
 				genes_sample_dict[gene_name] = 1
 	# Fill missing genes to dict
 	for gene in genes_of_interest:
@@ -67,16 +48,10 @@ for vcf_file in vcf_files_list:
 		else:
 			genes_sample_dict[gene] = 0
 
-	print("dict2")
-	print(genes_sample_dict)
-
-
 	# Append dict to dataframe: 
 	df = df.append(genes_sample_dict, ignore_index = True)
 
 
-
-
-print("final df")
+print("final df: ")
 print(df)
 
