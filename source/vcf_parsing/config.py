3fa1906609015792821dffde8ae53adb97d76674
David Lord <xlorda@medair1.medair.lcl>
2022-03-21 14:42:45 +0100

    test run variants-to-table.py

--- source/vcf_parsing/config.py
+++ source/vcf_parsing/config.py
@@ -1,10 +1,10 @@
 # May move the contents of this config file to the general project config later. 
 
 # Genes of interest:
-genes_of_interest = ['EGFR', 'BRAF', 'TP53', 'PAK3', 'ATRX']
+genes_of_interest = ['EGFR', 'KRAS', 'TP53', 'STK11', 'KEAP1', 'PTEN', 'ARID1A', 'ARID1B']
 
 # Working directory:
-WORK_DIR = '/home/xlorda'
+WORK_DIR = '/home/xlorda/anna_tmp/test_rerun/vcf_files'
 
 
 
