e434885116f866776941d0a6baa1d0860656badd
David Lord <xlorda@medair1.medair.lcl>
2022-04-08 12:33:07 +0200

    Fixed MarkDuplicates core-eater bug

--- source/BAM-file-processing/intermediate_file_cleanup.sh
+++ source/BAM-file-processing/intermediate_file_cleanup.sh
@@ -6,7 +6,7 @@
 #$ -cwd
 #$ -S /bin/bash
 #$ -pe mpi 10
-#$ -q batch.q
+#$ -q test.q
 #$ -o logs/stdout_BAM_processing_intermediate_file_cleanup.txt
 #$ -e logs/stderr_BAM_processing_intermediate_file_cleanup.txt
 
