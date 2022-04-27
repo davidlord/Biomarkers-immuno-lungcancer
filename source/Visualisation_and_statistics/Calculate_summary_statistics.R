0c1590db6e9e65d3bc1491121513387664b4928e
davidlord <david.lord96@gmail.com>
2022-04-27 18:15:22 +0200

    Added to visualization & summary stats script

--- source/Visualisation_and_statistics/Calculate_summary_statistics.R
+++ source/Visualisation_and_statistics/Calculate_summary_statistics.R
@@ -14,7 +14,7 @@ total_df <- read.delim("total_df.tsv", stringsAsFactors = TRUE)
 
 
 #=================================================================================
-# CALCULATE SUMMARY STATISTICS
+# CALCULATE SUMMARY STATISTICS ACROSS COHORTS
 #=================================================================================
 
 # Overview structure of dataset
@@ -77,91 +77,17 @@ table(total_df$Study_ID)
       )
       
   
-#=======================================================================  
-# SIMPLE BARPLOTS
 #=======================================================================
-
-# Study ID fractions (how large fractions derive from different data sets) barplot/pie chart.
-
-# Sequencing types barplot/pie charts.  
-
-# Smoking history distribution barplot. 
-      
-
-# Specific mutations
-
-      
-
-#=================================================================================
-# Visualizations
-#=================================================================================
-
-# NOTE: Rstudio can be a bit moody sometimes and not allow us to create the 
-# plots. If Rstudio is in a bad mood, you can generate the plots in a new script file
-# (running on the same instance).
-
-      
-
-# Heatmap of missing data:
-# DEV: May make this one prettier...
-missing_data <- vis_miss(total_df)
+# MUTATION FREQUENCIES
+#=======================================================================
+str(total_df)
+mutation_columns <- c('POLE', 'KEAP1', 'KRAS', 'POLD1', 'STK11', 'TP53', 'MSH2', 'EGFR', 'PTEN')
+# Subset df
+mutations_df <- total_df %>% select(Study_ID, mutation_columns)
 
 
 
-# Non-transformed histogram:
-TMB_hist <- total_df %>% ggplot(aes(x = TMB)) +
-  geom_histogram(binwidth = 1, fill = "mediumpurple2", col = "mediumpurple4") +
-  labs(x = 'Tumor Mutation Burden', y = 'Count')
-TMB_hist
-# Remove max TMB outlier from dataset:
-clinical_df <- clinical_df %>% filter(TMB < 75)
 
 
-# Log2-transformed histogram (excluding maximum outlier)
-TMB_hist_trans <- clinical_df %>% ggplot(aes(x = TMB)) +
-  geom_histogram(binwidth = 1, fill = "mediumpurple2", col = "mediumpurple4") +
-  labs(x = 'Tumor Mutation Burden (Log2 scale)', y = 'Count (Log2 scale)') +
-  scale_x_continuous(trans = "log2") +
-  scale_y_continuous(trans = "log2")
-TMB_hist_trans
 
 
-# TMB boxplot responders vs non-responders for each cohort
-TMB_boxp_DCB_vs_NCB <- clinical_df %>% ggplot(aes(x = Study_ID, y = TMB, fill = Durable_clinical_benefit)) +
-  geom_boxplot() +
-  scale_fill_brewer(palette = "Accent", direction = -1) +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-  labs(fill = "Durable Clinical Benefit", y = "Tumor Mutation Burden", x = "Cohort")
-TMB_boxp_DCB_vs_NCB
-
-# TMB boxplot responders vs non-responders for each cohort log2 transformed
-TMB_boxp_DCB_vs_NCB_log2 <- TMB_boxp_DCB_vs_NCB +
-  scale_y_continuous(trans = "log2") +
-  labs(y = "Tumor Mutation Burden (Log2 scale)")
-TMB_boxp_DCB_vs_NCB_log2
-
-
-# Boxplot comparison, TMB across sequencing methods
-## DEV: Reorder boxplots so that they are grouped by Sequencing type. 
-clinical_df %>% mutate(Study_ID = reorder(Study_ID, TMB, FUN = median)) %>%
-  ggplot(aes(x = Study_ID, y = TMB, fill = Sequencing_type)) +
-  geom_boxplot() +
-  #scale_y_continuous(trans = "log2") +
-  scale_fill_brewer(palette = "Accent") +
-  labs(fill = "Sequencing type", y = "Tumor Mutation Burden (Log2 scale)", x = "Cohort") +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
-
-
-# Grid of histograms displaying TMB from clinical data set (from cBioPortal), later also include Biolung data. 
-# Also include sequencing platform and N (sample size) as subtitle. 
-# Select cool colour. 
-df_cbioportal_clinical %>% ggplot(aes(TMB..nonsynonymous.)) + geom_histogram(binwidth = 1) + facet_wrap(~Study.ID)
-
-# Generate boxplots of TMB across different studies. Add gene panel and sample size as sub-title. Facet grid. 
-
-# Generate boxplots of TMB comparison between responders and non-responders in each study, facet grid. 
-
-
-# Generate barplots of comparison between sexes. 
-
-# Boxplot: Group by age. Facet by study. 
--- source/Visualisation_and_statistics/Summary_visualization.R
+++ source/Visualisation_and_statistics/Summary_visualization.R
@@ -5,6 +5,8 @@
 library(ggplot2)
 library(dplyr)
 library(tidyverse)
+library(naniar)
+library(visdat)
 
 # Set working directory (also place data to read in working directory).
 WORK_DIR <- "/Users/davidlord/Documents/External_data/script_running"
@@ -18,8 +20,10 @@ total_df <- read.delim("total_df.tsv", stringsAsFactors = TRUE)
 # PLOT RAWDATA
 #=======================================================================
 
+
+#=======================================================================
 # TMB HISTOGRAMS
-#================
+#=======================================================================
 # Histogram of raw TMB values
 TMB_hist <- total_df %>% ggplot(aes(x = TMB)) +
   geom_histogram(binwidth = 1, fill = "skyblue1", col = "skyblue4") +
@@ -43,21 +47,23 @@ TMB_hist_trans
 
 # TMB color by durable clinical benefit
 TMB_hist_clinical_outcome <- total_df %>% ggplot(aes(x = TMB, color = Durable_clinical_benefit, fill = Durable_clinical_benefit)) +
-  geom_histogram(binwidth = 1, position = "identity", alpha = 0.5) + 
-  scale_color_brewer(palette = "Accent", direction = -1) +
-  scale_fill_brewer(palette = "Accent", direction = -1)
+  geom_histogram(binwidth = 1, position = "identity", alpha = 0.9) + 
+  scale_color_brewer(palette = "Paired", direction = -1) +
+  scale_fill_brewer(palette = "Paired", direction = -1) +
+  labs(x = "Tumor Mutation Burden", size = 10)
 TMB_hist_clinical_outcome
 # No observed differnece in distribution of TMB between responders/non-responders comparing across cohorts. 
 
 
+#=======================================================================
 # TMB BOXPLOTS
-#===============
+#=======================================================================
 
 # Responders vs. non-responders, group by cohort
 TMB_by_cohort_boxplot <- total_df %>% mutate(Study_ID = reorder(Study_ID, TMB, FUN = median)) %>% ggplot(
   aes(x = Study_ID, y = TMB, fill = Durable_clinical_benefit)) +
   geom_boxplot() +
-  scale_fill_brewer(palette = "Accent", direction = -1) +
+  scale_fill_brewer(palette = "Paired", direction = -1) +
   labs(fill = "Durable Clinical Benefit", y = "Tumor Mutation Burden", x = "Cohort") +
   theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 10))
 TMB_by_cohort_boxplot
@@ -75,6 +81,7 @@ TMB_by_cohort <- total_df %>% mutate(Study_ID = reorder(Study_ID, TMB, FUN = med
   geom_boxplot() + 
   scale_y_continuous(trans = "log2") +
   labs(y = "Tumor Mutation Burden (Log2 scale)", x = "Cohort") +
+  scale_fill_brewer(palette = "Blues") +
   theme(legend.position="none") +
   theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 10))
 TMB_by_cohort
@@ -85,45 +92,60 @@ TMB_by_sequencing_type <- total_df %>% mutate(Sequencing_type = reorder(Sequenci
   geom_boxplot() + 
   scale_y_continuous(trans = "log2") + 
   labs(y = "Tumor Mutation Burden (Log2 scale)", x = "Sequencing type") + 
+  scale_fill_brewer(palette = "Blues") +
   theme(legend.position="none") +
   theme(axis.text.x = element_text(angle = 30, hjust = 1, size = 10))
 TMB_by_sequencing_type
 
 
+#=======================================================================
+# VISUALIZE MISSING DATA
+#=======================================================================
+
+# Convert empty entries to NA
+total_df <- total_df %>% mutate_all(na_if, "")
+
+# DEV: First deselect mutation columns
+
+# Heatmap of missing data (and percentages)
+vis_miss(total_df)
+
+# Barplots of missing data (counts)
+gg_miss_var(total_df) + 
+  labs(y = "Missing data")
 
 
+#=======================================================================
+# MUTATION FREQUENCIES
+#=======================================================================
+str(total_df)
+mutation_columns <- c('POLE', 'KEAP1', 'KRAS', 'POLD1', 'STK11', 'TP53', 'MSH2', 'EGFR', 'PTEN')
+# Subset df
+mutations_df <- total_df %>% select(Study_ID, mutation_columns)
 
-# Heatmap of missing data:
-# DEV: May make this one prettier...
-missing_data <- vis_miss(total_df)
+# Rename 0 1 to mutated not mutated
+ind <- mutations_df == 0
+mutations_df[ind] <- "Not mutated"
+ind <- mutations_df == 1
+mutations_df[ind] <- "Mutated"
 
-
-# Study ID fractions (how large fractions derive from different data sets) barplot/pie chart.
-
-# Sequencing types barplot/pie charts.  
-
-# Smoking history distribution barplot. 
-      
-
-# Specific mutations
-
-      
-
-#=================================================================================
-# Visualizations
-#=================================================================================
-
-# NOTE: Rstudio can be a bit moody sometimes and not allow us to create the 
-# plots. If Rstudio is in a bad mood, you can generate the plots in a new script file
-# (running on the same instance).
-
-      
+# Facet by gene. 
+# x = study, facet by gene. 
+mutations_df %>% ggplot(aes(x = Study_ID, y = ))
 
 
+#=======================================================================
+# MSI
+#=======================================================================
+biolung_df <- total_df %>% filter(Study_ID == "BioLung_2022")
 
-
-
-
+MSI_boxplot <- biolung_df %>% ggplot(aes(
+  x = Durable_clinical_benefit, y = MSI_MSISensorPro, fill = Durable_clinical_benefit)) +
+  geom_boxplot() +
+  scale_fill_brewer(palette = "Paired", direction = -1) + 
+  labs(x = "Durable clinical benefit", y = "% Microsatellite instability", subtitle = "N = 34", size = 10) +
+  theme(legend.position = "none")
+MSI_boxplot
 
 
 
