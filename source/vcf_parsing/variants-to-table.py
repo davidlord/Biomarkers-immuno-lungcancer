ce84dbec25380a5771ed147960b1dcf68fe0964f
davidlord <87085423+davidlord@users.noreply.github.com>
2022-05-17 07:26:07 +0200

    Update variants-to-table.py
    Added comments

--- source/vcf_parsing/variants-to-table.py
+++ source/vcf_parsing/variants-to-table.py
@@ -18,6 +18,7 @@ vcf_files_list = glob.glob("*.vcf")
 
 
 # Get sample names, store in list
+### DEV: Use regex instead of subsetting string when getting sample name
 sample_names_list = []
 for vcf_file in vcf_files_list:
 	samplename = vcf_file[:8]
@@ -34,23 +35,27 @@ df = pd.DataFrame(columns = column_names)
 # Parse through vcf files (cyvcf2): 
 for vcf_file in vcf_files_list:
 	# get sample name: 
-		# Development: Use split instead of hardcoding, update later. 
+	### DEV: Use split/regex instead of hardcoding, update later. 
 	samplename = vcf_file[:8]
 	# Create dict to store mutated genes:
 	genes_sample_dict = {"Patient.ID": samplename}
 	for variant in VCF(vcf_file):
+		# Note: None = PASS in CyVCF
 		if variant.FILTER != None:
 			continue
 		else:
 			infoF = variant.INFO.get('FUNCOTATION')
+			# Get gene name
 			gene_name = infoF.split('|')[0][1:]
 			if gene_name in genes_of_interest:
+				# If gene-mutation present, gene_name = 1
 				genes_sample_dict[gene_name] = 1
 	# Fill missing genes to dict
 	for gene in genes_of_interest:
 		if gene in genes_sample_dict:
 			continue
 		else:
+			# If gene-mutation not present, gene_name = 0
 			genes_sample_dict[gene] = 0
 
 	# Append dict to dataframe: 
